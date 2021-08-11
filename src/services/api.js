4b139483531b77801b543ac3b571ec0eb212bed7
Marcus Vinicius Mendes do Nascimento <marcus.mendes89@gmail.com>
2021-08-11 17:55:48 -0300

    Resolve ccnflito requisito 1

--- src/services/api.js
+++ src/services/api.js
@@ -7,29 +7,14 @@ export async function getCategories() {
   return object;
 }
 
-async function getProductsFromCategoryId(categoryId) {
-  const endpoint = 'https://api.mercadolibre.com/sites/MLB/search?category=';
-  const response = await fetch(`${endpoint}${categoryId}`);
-  const object = await response.json();
-
-  return object;
-}
-
-async function getProductsFromQuery(query) {
-  const endpoint = 'https://api.mercadolibre.com/sites/MLB/search?q=';
-  const response = await fetch(`${endpoint}${query}`);
-  const object = await response.json();
-
-  return object;
-}
 /* Lógica recomendada pelo Lucio no grupo de Whatsapp, para que a funçao possa ser chamada
 tanto pelo Id, quanto pela query */
 
 export async function getProductsFromCategoryAndQuery(categoryId, query) {
-  if (categoryId === null) {
-    return getProductsFromQuery(query);
-  }
-  if (query === null) {
-    return getProductsFromCategoryId(categoryId);
-  }
+  // Implemente aqui! Quando o fizer, descomente os parâmetros que essa função recebe
+  const url = `https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`;
+  const response = await fetch(url);
+  const object = await response.json();
+
+  return object;
 }
