3ef2a97a1d03a7309857a55439ea8e2469520f0e
diogodlemos <diogodrummond@hotmail.com>
2021-08-12 18:06:13 -0300

    Requisito 8 funcionando

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,18 +1,39 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import './ProductList.css';
+import { BrowserRouter } from 'react-router-dom';
 
 class ProductCard extends React.Component {
+  constructor() {
+    super();
+
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    const { product } = this.props;
+    localStorage.setItem('productAdd', product.title);
+  }
+
   render() {
     const { product: { title, thumbnail, price } } = this.props;
     return (
-      <div data-testid="product" className="card-list">
-        <div className="card-container">
-          <h2 className="card-title">{ title }</h2>
-          <img className="card-image" src={ thumbnail } alt={ title } />
-          <p className="card-price">{ price }</p>
+      <BrowserRouter>
+        <div data-testid="product" className="card-list">
+          <div className="card-container">
+            <h2 className="card-title">{ title }</h2>
+            <img className="card-image" src={ thumbnail } alt={ title } />
+            <p className="card-price">{ price }</p>
+            <button
+              type="button"
+              data-testid="product-add-to-cart"
+              onClick={ this.handleClick }
+            >
+              Adicionar ao Carrinho
+            </button>
+          </div>
         </div>
-      </div>
+      </BrowserRouter>
     );
   }
 }
