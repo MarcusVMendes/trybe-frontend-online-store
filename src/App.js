d9db7a38604e9dbc4275689789170b98901c611a
MarcusVMendes <marcus.mendes89@gmail.com>
2022-05-26 01:55:30 -0300

    Insert HashRouter

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { HashRouter as Router, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import ShoppingCart from './pages/ShoppingCart';
 import ProductDetails from './pages/ProductDetails';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Route exact path="/" component={ Home } />
-      <Route path="/cart" component={ ShoppingCart } />
-      <Route path="/details/:id" component={ ProductDetails } />
-    </BrowserRouter>
+    <Router basename={process.env.PUBLIC_URL}>
+      <Route exact path="/" component={Home} />
+      <Route path="/cart" component={ShoppingCart} />
+      <Route path="/details/:id" component={ProductDetails} />
+    </Router>
   );
 }
 
